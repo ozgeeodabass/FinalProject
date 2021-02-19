be7a0f8ad3a07cf304228d4c40fab47711215fe9
ÖZGE ODABAŞ <180315051@ogr.cbu.edu.tr>
2021-02-19 23:35:36 +0300

    Add project files.

---
+++ Business/Constans/Messages.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constans
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Product added";
+        public static string ProductNameInvalid = "Product name is invalid";
+        public static string MaintenanceTime= "System is in maintenance";
+        public static string ProductsListed= "Products are listed";
+    }
+}
